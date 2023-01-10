36356843fb86c279b8235e3fd05e3f65757af553
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-01-09 21:52:01 -0300

    add data layer

---
+++ Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Pedro Ivo de Faria Ramos on 09/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
