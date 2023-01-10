26e6c81095139e9c00d8b94983daaf9b4bc27b61
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-01-09 22:25:39 -0300

    chore: with public vars it needs to create a public constructor for structs

--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -9,4 +9,11 @@ public struct AddAccountModel {
     public var email: String
     public var password: String
     public var passwordConfirmation: String
+    
+    public init (name: String, email: String, password: String, passwordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
 }
