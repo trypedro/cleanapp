85794e71c579d7470bf9684f40610a52f1fb5962
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-01-10 15:11:18 -0300

    test: ensure AlamofireAdapter calls Alamofire with correct data

--- DataTests/Helpers/TestFactories.swift
+++ DataTests/Helpers/TestFactories.swift
@@ -4,6 +4,10 @@ func makeUrl() -> URL {
     return URL(string: "http://any-url.com")!
 }
 
+func makeValidData() -> Data {
+    return Data("{\"name\":\"Pedro\"}".utf8)
+}
+
 func makeInvalidData() -> Data {
     return Data("invalid_data".utf8)
 }
