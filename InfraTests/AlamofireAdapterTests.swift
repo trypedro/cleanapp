ab3620c0c412246c9804ffe3ffa21b54c70be4e5
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-01-10 06:44:21 -0300

    tests: create stub for request handler with alamofire

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -1,8 +1,42 @@
 import XCTest
+import Alamofire
+
+class AlamofireAdaptar {
+    private let session: Session
+
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
+    func post(to url: URL){
+        
+    }
+}
 
 class AlamofireAdapterTests: XCTestCase {
     func test_(){
-        
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdaptar(session: session)
+        sut.post(to: url)
     }
 
+}
+
+class URLProtocolStub: URLProtocol {
+    override open class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override open func startLoading() {
+        
+    }
+    
+    override open func stopLoading() {}
 }
