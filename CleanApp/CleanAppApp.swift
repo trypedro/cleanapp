a10b07b4e594f10c527ed94f64b11cd9310ac683
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-01-09 21:41:35 -0300

    Initial Commit

---
+++ CleanApp/CleanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanAppApp.swift
+//  CleanApp
+//
+//  Created by Pedro Ivo de Faria Ramos on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanApp
+//
+//  Created by Pedro Ivo de Faria Ramos on 09/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
