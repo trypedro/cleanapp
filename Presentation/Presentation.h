db38902bef5b5576659ac6270a82fd6fae02afc9
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-01-22 06:44:53 -0300

    feat: add presentation layer

---
+++ Presentation/Presentation.h
@@ -0,0 +1,18 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by Pedro Ivo de Faria Ramos on 22/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -7,7 +7,7 @@ class AddAccountIntegrationTests: XCTestCase {
 
     func test_add_account() {
         let alamofireAdapter = AlamofireAdaptar()
-        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
         let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
         let addAccountModel = AddAccountModel(name: "Pedro Ivo", email: "pedroivodefaria@gmail.com", password: "secret", passwordConfirmation: "secret")
         let exp = expectation(description: "waiting")
@@ -21,6 +21,6 @@ class AddAccountIntegrationTests: XCTestCase {
             }
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 10)
+        wait(for: [exp], timeout: 5)
     }
 }
