e4299be6e3771a0e42e8f95199105de56f53df4b
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-02-27 18:36:34 -0300

    test: ensure signUpPresenter hide isLoading after addAccountCompletes

--- PresentationTests/Presenters/SignUpPresenterTests.swift
+++ PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -107,16 +107,26 @@ class SignUpPresenterTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
     
-    func test_signUp_should_show_loading_before_call_addAccount() {
+    func test_signUp_should_show_loading_before_and_after_call_addAccount() {
         let loadingViewSpy = LoadingViewSpy()
-        let sut = makeSut(loadingView: loadingViewSpy)
+        let addAccountSpy = AddAccountSpy()
+        let sut = makeSut(addAccount: addAccountSpy, loadingView: loadingViewSpy)
         let exp = expectation(description: "waiting")
+        
         loadingViewSpy.observe { viewModel in
             XCTAssertEqual(viewModel, LoadingViewModel(isLoading: true))
             exp.fulfill()
         }
         sut.signUp(viewModel: makeSignUpViewModel())
         wait(for: [exp], timeout: 1)
+        let exp2 = expectation(description: "waiting")
+        
+        loadingViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel, LoadingViewModel(isLoading: false))
+            exp2.fulfill()
+        }
+        addAccountSpy.completeWithError(.unexpected)
+        wait(for: [exp2], timeout: 1)
     }
 }
 
