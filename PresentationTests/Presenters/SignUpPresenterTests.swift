d11604d4721105ff2920fc5492771b4ecff96292
Pedro  Ivo <pedroivodefaria@gmail.com>
2023-02-27 16:26:18 -0300

    chore: move presentation classes to their own files

--- PresentationTests/Presenters/SignUpPresenterTests.swift
+++ PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -1,48 +1,5 @@
 import XCTest
-
-class SignUpPresenter {
-    private let alertView: AlertView
-    
-    init (alertView: AlertView) {
-        self.alertView = alertView
-    }
-    
-    func signUp(viewModel: SignUpViewModel) {
-        if let message = validate(viewModel: viewModel) {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
-        }
-    }
-    
-    private func validate(viewModel: SignUpViewModel) -> String? {
-        if viewModel.name == nil || viewModel.name!.isEmpty {
-            return "Nome é um campo obrigatorio"
-        } else if viewModel.email == nil || viewModel.email!.isEmpty {
-            return "Email é um campo obrigatorio"
-        } else if viewModel.password == nil || viewModel.password!.isEmpty {
-            return "Password é um campo obrigatorio"
-        } else if viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty {
-            return "PasswordConfirmation é um campo obrigatorio"
-        }
-        return nil
-    }
-
-}
-
-protocol AlertView {
-    func showMessage(viewModel: AlertViewModel)
-}
-
-struct AlertViewModel: Equatable {
-    var title: String
-    var message: String
-}
-
-struct SignUpViewModel {
-    var name: String?
-    var email: String?
-    var password: String?
-    var passwordConfirmation: String?
-}
+import Presentation
 
 class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_error_message_if_name_is_not_provided() {
@@ -54,7 +11,7 @@ class SignUpPresenterTests: XCTestCase {
     
     func test_signUp_should_show_error_message_if_email_is_not_provided() {
         let (sut, alertViewSpy) = makeSut()
-        let signUpViewModel = SignUpViewModel(name: "any_name", password: "any_password", passwordConfirmation: "any_password")
+        let signUpViewModel = SignUpViewModel(name: "any_name", password: "any_password", passwordConfirmation: "any_passwor22d")
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Email é um campo obrigatorio"))
     }
