9517a5db7b12e986ac04a3441667508e02ad6b8d
Pedro Ivo <pedro.faria.ivo@gmail.com>
2023-03-03 17:55:50 -0300

    end email validator adapter

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -1,15 +1,5 @@
 import XCTest
-import Presentation
-
-class EmailValidatorAdapter: EmailValidator {
-    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    
-    func isValid(email: String) -> Bool {
-        let range = NSRange(location: 0, length: email.utf16.count)
-        let regex = try! NSRegularExpression(pattern: pattern)
-        return regex.firstMatch(in: email, range: range) != nil
-    }
-}
+import Validation
 
 final class EmailValidatorAdapterTests: XCTestCase {
     func test_invalid_emails() {
