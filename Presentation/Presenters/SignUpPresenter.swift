fb20e876c9bb6fc6a4585fd876aa5aead795407a
Pedro Ivo <pedro.faria.ivo@gmail.com>
2023-03-03 14:40:13 -0300

    chore: move signUpViewModel to its own file

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -50,16 +50,3 @@ public final class SignUpPresenter {
 
 }
 
-public struct SignUpViewModel {
-    public var name: String?
-    public var email: String?
-    public var password: String?
-    public var passwordConfirmation: String?
-    
-    public init(name: String? = nil, email: String? = nil, password: String? = nil, passwordConfirmation: String?  = nil) {
-        self.name = name
-        self.email = email
-        self.password = password
-        self.passwordConfirmation = passwordConfirmation
-    }
-}
